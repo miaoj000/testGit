cab813e4437064a96d150296665e978257133069
miaojj <1712446101@qq.com>
2021-12-05 16:24:56 -0800

    fir commit m

---
+++ testmongos.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
+const schema = {name: String,age:Number,health:String}
+const cats = mongoose.model('cats', schema)
+
+// const kitty = new Cat({ name: 'zhao'})
+// kitty.save().then(() => console.log('testzhao'))
+// const kitty1 = new Cat({name:"zhang", age:3, health:'great'})
+// kitty1.save().then(() => console.log('testzhang'))
+cats.find({name:'zhang'},(err,data)=>{
+    console.log(data[0]._doc.name)
+})
